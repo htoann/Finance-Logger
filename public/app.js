8a36b61d04bf03ad4ce476badbb494985377f3e0
bloghtoan <bloghtoan@gmail.com>
2021-06-28 12:53:48 +0700

    render

--- public/app.js
+++ public/app.js
@@ -1,2 +1,23 @@
-"use strict";
-var char = "Nice";
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+var form = document.querySelector(".new-item-form");
+// inputs
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+// list template instance
+var ul = document.querySelector("ul");
+var list = new ListTemplate(ul);
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, "end");
+});
---
+++ public/classes/Payment.js
@@ -0,0 +1,12 @@
+var Payment = /** @class */ (function () {
+    function Payment(recipient, details, amount) {
+        this.recipient = recipient;
+        this.details = details;
+        this.amount = amount;
+    }
+    Payment.prototype.format = function () {
+        return this.recipient + " is owed \u00A3" + this.amount + " for " + this.details;
+    };
+    return Payment;
+}());
+export { Payment };
